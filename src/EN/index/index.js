0686ac45c8379ebc2793d3b54043d9a2226440db
feinoko <feinoko@gmail.com>
2021-03-11 06:49:53 +0100

    progress:refactored bem

--- src/EN/index/index.js
+++ src/EN/index/index.js
@@ -1,2 +1,15 @@
 import 'regenerator-runtime/runtime' // required when using async/await with parcel
 import '../../css/styles.scss' // styles entry point
+
+// nav dropdown menu 
+
+// const shop_EL = document.querySelector('.site-header__list-item--dropdown');
+// const dropdown_EL = document.querySelector('.site-header__shop-dropdown');
+// // display on hovering shop
+// shop_EL.addEventListener('mouseenter', () => {
+// dropdown_EL.style.display = 'block';
+// })
+// // disappear on un-hover
+// shop_EL.addEventListener('mouseleave', () => {
+//   dropdown_EL.style.display = 'none';
+// })
