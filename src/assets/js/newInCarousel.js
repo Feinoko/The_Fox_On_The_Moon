34337c48dfecb7848b35fb29d58d65e03a3b2c95
feinoko <feinoko@gmail.com>
2021-04-11 07:39:40 +0200

    desktop carousel slider limit seems ok

--- src/assets/js/newInCarousel.js
+++ src/assets/js/newInCarousel.js
@@ -26,6 +26,7 @@ const slider_EL = document.querySelector('.new-in__slider');
 const rightArrow_EL = document.querySelector('.new-in__right');
 const leftArrow_EL = document.querySelector('.new-in__left');
 const panels_EL = document.querySelectorAll('.new-in__panels');
+const howManyPanels = panels_EL.length;
 
 // keep record of number of clicks on right or left arrow, to determine limit after which event not triggered, due to user having reached end
 let counter = 0;
@@ -43,23 +44,59 @@ slider_EL.onclick = (e) => {
     const panelPositionProperty = window.getComputedStyle(panel);
     let position = Number(panelPositionProperty.getPropertyValue('--position'));
 
-    // update 'position' custom inline css var on each panel (for next click)
+    // update 'position' custom inline css var on each panel (for next click):
+
     // -100% if click right arrow
     if (e.target.classList.contains('new-in__right')) {
-      position -= 50;
-      panel.setAttribute('style', `--position:${position}`);
+      if (canSlideRight) { // allow event handler unless user has reached end of carousel
+        canSlideLeft = true; // enables sliding left next time
+        leftArrow_EL.style.color = '#173336'; // left arrow becomes clickable again
+        position -= 50;
+        panel.setAttribute('style', `--position:${position}`);
+      }
       // +100% if click left arrow
     } else if (e.target.classList.contains('new-in__left')) {
-      position += 50;
-      console.log('position value: ' + position);
-      panel.setAttribute('style', `--position:${position}`);
+      if (canSlideLeft) { // allow event handler unless user has reached end of carousel
+        canSlideRight = true; // enables sliding left next time
+        rightArrow_EL.style.color = '#173336'; // right arrow becomes clickable again
+        position += 50;
+        console.log('position value: ' + position);
+        panel.setAttribute('style', `--position:${position}`);
+      }
+
     }
 
     // perform the translation
     const newPositionTranslate = `translateX(${position}%)`;
     console.log(position);
     panel.style.transform = newPositionTranslate;
-  })
+  }) // end foreach
+
+  // check if user has reached end panel:
+
+  // if reached right limit
+  if (e.target.classList.contains('new-in__right')) {
+    counter++;
+    console.log(`counter: ${counter}`);
+    if (counter >= howManyPanels - 1) {
+      counter = howManyPanels - 1;
+      canSlideRight = false;
+      console.log(`canSlideright set to false`);
+      rightArrow_EL.style.color = 'rgb(170, 170, 170)';
+    }
+  }
+  // if reached left limit
+  if (e.target.classList.contains('new-in__left')) {
+    counter--;
+    console.log(`counter: ${counter}`);
+    if (counter <= 0) {
+      counter = 0;
+      canSlideLeft = false;
+      console.log(`canSlideleft set to false`);
+      leftArrow_EL.style.color = 'rgb(170, 170, 170)';
+    }
+  }
+
 }
 
 
@@ -155,7 +192,7 @@ parent.onclick = (e) => {
       console.log(`counter: ${counter}`);
       if (counter === numberOfPanels) {
         canSlideRight = false;
-        console.log(`canSlide set to false`);
+        console.log(`canSlideRight set to false`);
         mobRightArrow_EL.style.color = 'rgb(170, 170, 170)';
       }
     }
@@ -199,7 +236,7 @@ parent.onclick = (e) => {
       console.log(`counter: ${counter}`);
       if (counter === 0) {
         canSlideLeft = false;
-        console.log(`canSlide set to false`);
+        console.log(`canSlideLeft set to false`);
         mobLeftArrow_EL.style.color = 'rgb(170, 170, 170)';
       }
     }
@@ -228,13 +265,18 @@ window.addEventListener('resize', propertyResetter);
 // media query, trigger when screen below 500px
 function propertyResetter() {
 
-  /* overrides inline css variable on mobile version that controls translation (no more slider system) so that subsequent panels are not kept translated, in event that user slides panels then for some reason reduces screen size under 500px (switching from landscape to upright on mobile!) */
+  // resets translates, resets carousel position to origin by resetting opacity (1st panel has 1, others have 0)
   if (mediaQuery.matches) {
     console.log('hello');
     panels_EL.forEach((panel) => {
       panel.style.transform = 'translateX(0)';
+      panel.style.opacity = '0';
       wasMobile = true;
     })
+    panels_EL[0].style.opacity = '1';
+
+    // counter keeps tracks of whether user has reached end of carousels (to grey out arrow and prevent further sliding)
+    counter = 0;
   }
 
   // reset translateX inline css & custom var --translate to initial value in case user enlarges screen again above 500px (otherwise messes up slider system panels)
